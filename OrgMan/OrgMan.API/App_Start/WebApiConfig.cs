feb5cadf3cdcac5908d55666f21904af1431229a
Jonas Hodel <jonas.hodel@gmx.ch>
2017-11-09 22:46:25 +0100

    create Serversided Projects

---
+++ OrgMan/OrgMan.API/App_Start/WebApiConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace OrgMan.API
+{
+    public static class WebApiConfig 
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web-API-Konfiguration und -Dienste
+
+            // Web-API-Routen
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}

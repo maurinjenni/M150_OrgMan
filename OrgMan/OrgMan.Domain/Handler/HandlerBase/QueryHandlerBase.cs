886775c48e7556242683df14c99bff17c4bf3647
Jonas Hodel <jonas.hodel@gmx.ch>
2017-11-29 22:51:58 +0100

    Implement QueryHandlerBase

---
+++ OrgMan/OrgMan.Domain/Handler/HandlerBase/QueryHandlerBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity;
+
+namespace OrgMan.Domain.Handler.HandlerBase
+{
+    public class QueryHandlerBase
+    {
+        private IUnityContainer _unityContainer;
+
+        public QueryHandlerBase(IUnityContainer unityContainer)
+        {
+            _unityContainer = unityContainer;
+        }
+    }
+}

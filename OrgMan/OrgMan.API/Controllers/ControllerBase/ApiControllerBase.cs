a37c9a8b2c6e7a24346cd789cabbd289a1c3578e
Jonas Hodel <jonas.hodel@gmx.ch>
2017-11-29 23:22:23 +0100

    CreatedHttp Module for Authentication.... need to call Module

---
+++ OrgMan/OrgMan.API/Controllers/ControllerBase/ApiControllerBase.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace OrgMan.API.Controllers.ControllerBase
+{
+    public class ApiControllerBase : ApiController
+    {
+        public static IUnityContainer UnityContainer { get; set; }
+    }
+}
